21715fd7d2858f405defde56f3d7d1e7e834c308
paev-odoo <165154767+paev-odoo@users.noreply.github.com>
2026-08-21 10:39:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from pathlib import Path
+from platformdirs import user_downloads_dir
 import qrcode
 
 url = input("Enter the link: ")
 filename = input("Enter the file name: ")
 
-# Save directly to the user's Downloads folder
-downloads_folder = Path.home() / "Downloads"
-save_path = downloads_folder / f"{filename}.png"
+# Automatically detects system language & custom download paths
+save_path = Path(user_downloads_dir()) / f"{filename}.png"
 
 img = qrcode.make(url)
 img.save(save_path)
